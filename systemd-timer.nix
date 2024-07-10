c3d711b79c0661e9e50393c775d4a8ff74bb19c1
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2024-07-10 18:20:55 +0545

    fix: disable auto brightness for now

--- systemd-timer.nix
+++ systemd-timer.nix
@@ -2,17 +2,17 @@
 
 {
   systemd = {
-    timers.simple-timer = {
-      wantedBy = [ "timers.target" ];
-      partOf = [ "simple-timer.service" ];
-      timerConfig = {
-        OnBootSec = "5m"; # 5 minutes after boot
-        OnCalendar = "*-*-* *:30:00"; # Every hour at half past e.g. 12:30, 13:30, 14:30 and so on.
-      };
-    };
-    services.simple-timer = {
-      serviceConfig.Type = "oneshot";
-      script = (builtins.readFile ./set-brightness.sh);
-    };
+#    timers.simple-timer = {
+#      wantedBy = [ "timers.target" ];
+#      partOf = [ "simple-timer.service" ];
+#      timerConfig = {
+#        OnBootSec = "5m"; # 5 minutes after boot
+#        OnCalendar = "*-*-* *:30:00"; # Every hour at half past e.g. 12:30, 13:30, 14:30 and so on.
+#      };
+#    };
+#    services.simple-timer = {
+#      serviceConfig.Type = "oneshot";
+#      script = (builtins.readFile ./set-brightness.sh);
+#    };
   };
 }
