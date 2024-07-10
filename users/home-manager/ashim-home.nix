f825b7f3c4e7d8baf4f91ab7f7d4c2ec2fd6126c
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2024-07-10 18:11:40 +0545

    refactor: ashim-home

--- users/home-manager/ashim-home.nix
+++ users/home-manager/ashim-home.nix
@@ -8,35 +8,32 @@
     ./dconf.nix
   ];
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "ashim";
-  home.homeDirectory = "/home/ashim";
+  home = {
+    stateVersion = "24.05";
+    username = "ashim";
+    homeDirectory = "/home/ashim";
 
-  home.packages = with pkgs; [
-    qbittorrent
-    sublime4
-    protonvpn-gui
-    shotwell
-    libreoffice
-    jetbrains.idea-ultimate
-    deja-dup
-    gparted
-    veracrypt
-    ventoy-full
-    neofetch
-    aria2
-    persepolis #GUI for aria2
-    zip
-    unzip
+    packages = with pkgs; [
+      qbittorrent
+      sublime4
+      protonvpn-gui
+      shotwell
+      libreoffice
+      jetbrains.idea-ultimate
+      deja-dup
+      gparted
+      veracrypt
+      ventoy-full
+      neofetch
+      aria2
+      persepolis #GUI for aria2
+      zip
+      unzip
 
-    ddcutil #gnome brightness control
-  ];
+      ddcutil #gnome brightness control
+    ];
+  };
 
-  # The state version is required and should stay at the version you
-  # originally installed.
-  home.stateVersion = "24.05";
-  
   programs = {
     home-manager.enable = true; # Let Home Manager install and manage itself.
     git = {
@@ -45,13 +42,4 @@
       userEmail = "5734294+ashimregme@users.noreply.github.com";
     };
   };
-
-  # systemd.user.services.custombrightness = {
-  #   description = "Change brightness of monitor according to the time of the day";
-  #   script = ''
-      
-  #   '';
-  #   wantedBy = [ "graphical-session.target" ];
-  #   partOf = [ "graphical-session.target" ];
-  # };
 }
