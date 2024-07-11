df02f610c7bcc1c9b3da4cf7a626ebffc21cbc52
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2024-07-11 22:58:43 +0545

    feat: add ulauncher

--- users/home-manager/ashim-home.nix
+++ users/home-manager/ashim-home.nix
@@ -1,45 +1,60 @@
-{ config, pkgs, ... }:
+{ config, pkgs, systemd, ... }:
 
 {
-  imports = [
-    ./firefox/firefox.nix
-    ./zsh.nix
-    ./terminal.nix
-    ./dconf.nix
-  ];
-
-  home = {
-    stateVersion = "24.05";
-    username = "ashim";
-    homeDirectory = "/home/ashim";
-
-    packages = with pkgs; [
-      qbittorrent
-      sublime4
-      protonvpn-gui
-      shotwell
-      libreoffice
-      jetbrains.idea-ultimate
-      deja-dup
-      gparted
-      veracrypt
-      ventoy-full
-      neofetch
-      aria2
-      persepolis #GUI for aria2
-      zip
-      unzip
-
-      ddcutil #brightness control
-    ];
+  users.users.ashim = {
+    isNormalUser = true;
+    description = "Ashim Regmi";
+    extraGroups = [ "networkmanager" "wheel" "docker" "i2c" ];
+    initialPassword = "changeme";
+    shell = pkgs.zsh;
   };
 
-  programs = {
-    home-manager.enable = true; # Let Home Manager install and manage itself.
-    git = {
-      enable = true;
-      userName = "Ashim Regmi";
-      userEmail = "5734294+ashimregme@users.noreply.github.com";
+  home-manager.users.ashim = {
+    imports = [
+      ./firefox/firefox.nix
+      ./zsh.nix
+      ./terminal.nix
+      ./dconf.nix
+    ];
+
+    home = {
+      stateVersion = "24.05";
+      username = "ashim";
+      homeDirectory = "/home/ashim";
+
+      packages = with pkgs; [
+        qbittorrent
+        sublime4
+        protonvpn-gui
+        shotwell
+        libreoffice
+        jetbrains.idea-ultimate
+        deja-dup
+        gparted
+        veracrypt
+        ventoy-full
+        neofetch
+        aria2
+        persepolis #GUI for aria2
+        zip
+        unzip
+        ddcutil #brightness control
+
+        ulauncher
+      ];
+    };
+
+    programs = {
+      home-manager.enable = true; # Let Home Manager install and manage itself.
+      git = {
+        enable = true;
+        userName = "Ashim Regmi";
+        userEmail = "5734294+ashimregme@users.noreply.github.com";
+      };
     };
   };
+
+  # Equivalent to command 'systemctl --user enable ulauncher'
+  systemd.packages = with pkgs; [ ulauncher ];
+  systemd.user.services.ulauncher.wantedBy = [ "default.target" ];
 }
--- users/users.nix
+++ users/users.nix
@@ -1,19 +1,12 @@
 { config, pkgs, inputs, ... }:
 
 {
-  users.users.ashim = {
-    isNormalUser = true;
-    description = "Ashim Regmi";
-    extraGroups = [ "networkmanager" "wheel" "docker" "i2c" ];
-    initialPassword = "changeme";
-    shell = pkgs.zsh;
-  };
+  imports = [ ./home-manager/ashim-home.nix ];
 
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
     useUserPackages = true;
     useGlobalPkgs = true;
     backupFileExtension = "backup";
-    users.ashim = import ./home-manager/ashim-home.nix;
   };
 }
