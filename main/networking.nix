6452bdb1e18cfba80c2c8d44c34d9296834492ba
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2024-07-13 16:04:24 +0545

    feat: move resolved configuration to networking.nix

--- main/networking.nix
+++ main/networking.nix
@@ -15,4 +15,19 @@
       ];
     };
   };
+
+  services.resolved = {
+    enable = false;
+    dnssec = "true";
+    dnsovertls = "true";
+    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+    extraConfig = ''
+      ReadEtcHosts=no
+      # DNS=127.0.0.1:5053
+      DNS=192.168.11.1  # router IP, assuming https-dns-proxy is setup
+      DNSStubListener=yes
+      # DNSStubListener=no
+      DNSSEC=no
+    '';
+  };
 }
