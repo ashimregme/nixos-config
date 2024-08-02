96fa55213a5447d7cfd3d397bee1b6834b558813
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2024-08-02 08:52:28 +0545

    fix: switching back to gnome only - removed specialisation

--- services/de.nix
+++ services/de.nix
@@ -1,42 +1,31 @@
 { config, pkgs, ... }:
 
 {
-  specialisation = {
-    kde.configuration = {
-      services.displayManager.sddm.enable = true;
-      services.desktopManager.plasma6.enable = true;
+  specialisation = {};
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
-      environment.plasma6.excludePackages = with pkgs.kdePackages; [
-      ];
-    };
-
-    gnome.configuration = {
-      services.xserver.displayManager.gdm.enable = true;
-      services.xserver.desktopManager.gnome.enable = true;
-
-      services.gnome = {
-        core-utilities.enable = false;
-        tracker-miners.enable = false;
-        tracker.enable = false;
-        gnome-browser-connector.enable = true;
-      };
-
-      environment.systemPackages = with pkgs; [
-        gnomeExtensions.appindicator
-        gnomeExtensions.gsconnect
-        gnome.gnome-calculator
-        gnome.gnome-system-monitor
-        gnome.gnome-disk-utility
-        gnome.nautilus
-        gnome.eog
-        gnome.gnome-clocks
-      ];
-
-      environment.gnome.excludePackages = with pkgs.gnome; [
-        pkgs.gnome-tour
-      ];
-
-      services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
-    };
+  services.gnome = {
+    core-utilities.enable = false;
+    tracker-miners.enable = false;
+    tracker.enable = false;
+    gnome-browser-connector.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.appindicator
+    gnomeExtensions.gsconnect
+    gnome.gnome-calculator
+    gnome.gnome-system-monitor
+    gnome.gnome-disk-utility
+    gnome.nautilus
+    gnome.eog
+    gnome.gnome-clocks
+  ];
+
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    pkgs.gnome-tour
+  ];
+
+  services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
 }
