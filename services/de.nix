78d3f70b5c67600752777f87f2e6262aa8968663
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2024-12-01 11:09:44 +0545

    gnome package changes in 24.11

--- services/de.nix
+++ services/de.nix
@@ -8,25 +8,26 @@
   services.gnome = {
     core-utilities.enable = false;
     tracker-miners.enable = false;
-    tracker.enable = false;
+    localsearch.enable = false;
+    tinysparql.enable = false;
     gnome-browser-connector.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
     gnomeExtensions.appindicator
     gnomeExtensions.gsconnect
-    gnome.gnome-calculator
-    gnome.gnome-system-monitor
-    gnome.gnome-disk-utility
-    gnome.nautilus
-    gnome.eog
-    gnome.gnome-clocks
-    gnome.gnome-tweaks
+    gnome-calculator
+    gnome-system-monitor
+    gnome-disk-utility
+    nautilus
+    eog
+    gnome-clocks
+    gnome-tweaks
   ];
 
   environment.gnome.excludePackages = with pkgs.gnome; [
     pkgs.gnome-tour
   ];
 
-  services.udev.packages = [ pkgs.gnome.gnome-settings-daemon ];
+  services.udev.packages = [ pkgs.gnome-settings-daemon ];
 }
