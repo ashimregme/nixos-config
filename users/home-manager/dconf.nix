e71d5ae97289bdffb338255cb5fc36342fec02c7
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2024-12-22 18:10:09 +0545

    update dconf settings for gnome

--- users/home-manager/dconf.nix
+++ users/home-manager/dconf.nix
@@ -40,16 +40,60 @@
           "org.qbittorrent.qBittorrent.desktop"
         ];
       };
+
+      "org/gnome/mutter" = {
+        attach-modal-dialogs = true;
+        center-new-windows = true;
+        dynamic-workspaces = true;
+        edge-tiling = true;
+        focus-change-on-pointer-rest = true;
+        workspaces-only-on-primary = true;
+        experimental-features = ["scale-monitor-framebuffer" "xwayland-native-scaling"];
+      };
+
       "org/gnome/desktop/interface" = {
-        clock-format = "24h";
         clock-show-weekday = true;
+        enable-animations = true;
         enable-hot-corners = true;
+        font-antialiasing = "grayscale";
+        font-hinting = "slight";
+        gtk-im-module = "gtk-im-context-simple";
+        locate-pointer = true;
         show-battery-percentage = true;
+        toolkit-accessibility = false;
       };
 
       "org/gnome/desktop/sound" = {
         allow-volume-above-100-percent = true;
         event-sounds = true;
+      };
+
+      "org/gnome/desktop/peripherals/touchpad" = {
+        click-method = "areas";
+        tap-to-click = true;
+        two-finger-scrolling-enabled = true;
+      };
+
+      "org/gnome/settings-daemon/plugins/power" = {
+        power-button-action = "interactive";
+        sleep-inactive-ac-type = "nothing";
+        sleep-inactive-battery-timeout = 1800;
+      };
+
+      "org/gnome/settings-daemon/plugins/color" = {
+        night-light-enabled = true;
+        night-light-schedule-automatic = true;
+        night-light-schedule-from = 18.0;
+        night-light-schedule-to = 5.0;
+        night-light-temperature = 500;
+      };
+
+      "org/gnome/desktop/privacy" = {
+        disable-microphone = false;
+        old-files-age = 7;
+        remember-recent-files = false;
+        remove-old-temp-files = true;
+        remove-old-trash-files = true;
       };
 
       # To avoid "no default hypervisor" warning, see: https://nixos.wiki/wiki/Virt-manager
