7e7568b0363377870a77ad599f56a7aca53359fe
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-03-29 20:41:08 +0545

    fix autostart apps

--- modules/home/autostart.nix
+++ modules/home/autostart.nix
@@ -1,29 +1,27 @@
 { config, pkgs, inputs, username, ... }:
 
 {
-  imports = [ inputs.home-manager.nixosModules.home-manager ];
-
-  file."${config.home-manager.users.${username}.xdg.configHome}/autostart/protonvpn-app.desktop" = {
+  xdg.configFile."autostart/protonvpn-app.desktop" = {
     enable = true;
     source = "${pkgs.protonvpn-gui}/share/applications/protonvpn-app.desktop";
   };
 
-  file."${config.home-manager.users.${username}.xdg.configHome}/autostart/thunderbird.desktop" = {
+  xdg.configFile."autostart/thunderbird.desktop" = {
     enable = true;
     source = "${pkgs.thunderbird}/share/applications/thunderbird.desktop";
   };
 
-  file."${config.home-manager.users.${username}.xdg.configHome}/autostart/firefox.desktop" = {
+  xdg.configFile."autostart/firefox.desktop" = {
     enable = true;
     source = "${pkgs.firefox}/share/applications/firefox.desktop";
   };
 
-  file."${config.home-manager.users.${username}.xdg.configHome}/autostart/guake.desktop" = {
+  xdg.configFile."autostart/guake.desktop" = {
     enable = true;
     source = "${pkgs.guake}/share/applications/guake.desktop";
   };
 
-  file."${config.home-manager.users.${username}.xdg.configHome}/autostart/idea-ultimate.desktop" = {
+  xdg.configFile."autostart/idea-ultimate.desktop" = {
     enable = true;
     source = "${pkgs.guake}/share/applications/idea-ultimate.desktop";
   };
