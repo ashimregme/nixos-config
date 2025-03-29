49a2ff10b6353c127f68c0cd698c20920545f35a
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-03-29 21:37:59 +0545

    re-enable gdm and gnome services

--- modules/core/services/de.nix
+++ modules/core/services/de.nix
@@ -2,8 +2,8 @@
 
 {
   specialisation = {};
-#  services.xserver.displayManager.gdm.enable = true;
-#  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   services.gnome = {
     core-utilities.enable = false;
