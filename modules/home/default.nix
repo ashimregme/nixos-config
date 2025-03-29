0b1d0170e7ed24bebf88cc6b5315ee637d508225
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-03-29 21:35:30 +0545

    fix autostart apps

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -7,7 +7,7 @@
     ./terminal.nix                    # shell
     ./dconf.nix                       # configurations
     ./packages.nix                    # user packages not available in NixOS by default
-#    ./autostart.nix
+    ./autostart.nix
     ./programs.nix                    # user programs available in NixOS by default
     ./hyprland                        # window manager
     ./waybar                          # status bar
