77c535a65a9e534d5a07d1c8471f0d747b234071
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-03-30 21:29:19 +0545

    enable openssh daemon

--- modules/core/services/default.nix
+++ modules/core/services/default.nix
@@ -13,7 +13,7 @@
     libinput.enable = true; # Touchpad support (enabled by default in most desktopManager).
     journald.storage = "volatile"; # Store journald logs only in RAM (to be kind to ssd)
 
-    openssh.enable = false; # Enable the OpenSSH daemon.
+    openssh.enable = true; # Enable the OpenSSH daemon.
     logrotate.checkConfig = false; # temporary solution (https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000/28501)
   };
 }
