be9675af5c65b62ecf714bca895ce83c1666ab6a
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-06-21 12:36:22 +0545

    rebuild optimization

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -18,6 +18,13 @@
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+    # This allows Nix to download and build multiple derivations in parallel.
+    # A good starting point is the number of your CPU cores, or a number
+    # like 4 or 8 if you have a fast network connection.
+    max-jobs = 4;
+    # `0` means auto-detect based on the number of cores.
+    # This is for building, not downloading, but often set at the same time.
+    cores = 0;
   };
 
   documentation.nixos.enable = false;
