8eb6ff6b52f9fc20bb396b71621dfa26c1c24b07
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-07-12 21:58:26 +0545

    allow tailscale and php server 8080 ports on firewall

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -7,8 +7,12 @@
     networkmanager.enable = true; # Enable networking
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 8096 8920 4200 ];
-      allowedUDPPorts = [ 1900 7359 ];
+      allowedTCPPorts = [ 8096 8920 4200 8080];
+      allowedUDPPorts = [
+        1900
+        7359
+        41641 #tailscale
+      ];
       allowedTCPPortRanges = [
         { from = 1714; to = 1764; } # GSConnect ports
       ];
