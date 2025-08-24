2275456c18eb3ca3d758247d9ffa387167c4db76
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-08-24 19:32:11 +0545

    enable thermald service

--- modules/core/services/default.nix
+++ modules/core/services/default.nix
@@ -17,5 +17,6 @@
     logrotate.checkConfig = false; # temporary solution (https://discourse.nixos.org/t/logrotate-config-fails-due-to-missing-group-30000/28501)
     tailscale.enable = false;
     pulseaudio.enable = false;
+    thermald.enable = true; # proactively prevents overheating on Intel CPUs and works well with other tools. (https://wiki.nixos.org/wiki/Laptop)
   };
 }
