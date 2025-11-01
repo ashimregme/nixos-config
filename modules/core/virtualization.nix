dc5277c75b1eadaae0124e1a90043e60a141453e
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2025-11-01 16:01:17 +0545

    configure NIX_CONF_DIR session env var

--- modules/core/virtualization.nix
+++ modules/core/virtualization.nix
@@ -5,7 +5,10 @@
   ...
 }:
 {
-  environment.sessionVariables.LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
+  environment.sessionVariables = {
+    LIBVIRT_DEFAULT_URI = [ "qemu:///system" ];
+    NIX_CONF_DIR = "/home/ashim/nixos-config";
+  };
   virtualisation = {
     docker = {
       enable = true;
