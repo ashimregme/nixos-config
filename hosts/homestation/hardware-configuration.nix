2ccf7f2285beffdf78a8327eebc062efa7b1c456
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2026-01-13 09:19:30 +0545

    changing to new hardware

--- hosts/homestation/hardware-configuration.nix
+++ hosts/homestation/hardware-configuration.nix
@@ -4,34 +4,33 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "uas" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/24d4fc9a-29ea-4f3d-8981-c756e966d26b";
+    { device = "/dev/mapper/luks-1f063d49-e190-4eac-a9d6-9d04b035fbb5";
       fsType = "ext4";
     };
 
-  boot.initrd.luks.devices."luks-109c109d-965a-4a0a-9e4e-e032051512bf".device = "/dev/disk/by-uuid/109c109d-965a-4a0a-9e4e-e032051512bf";
+  boot.initrd.luks.devices."luks-1f063d49-e190-4eac-a9d6-9d04b035fbb5".device = "/dev/disk/by-uuid/1f063d49-e190-4eac-a9d6-9d04b035fbb5";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/6736-FDE0";
+    { device = "/dev/disk/by-uuid/2034-13F9";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   # Hibernation - Start
-  boot.kernelParams = ["resume_offset=1681408"]; # sudo filefrag -v /var/lib/swapfile | head
-  boot.resumeDevice = "/dev/disk/by-uuid/24d4fc9a-29ea-4f3d-8981-c756e966d26b"; # lsblk -f (root UUID)
+  boot.kernelParams = ["resume_offset=28624896"]; # sudo filefrag -v /var/lib/swapfile | head
+  boot.resumeDevice = "/dev/disk/by-uuid/23fc474fa-2e1a-480c-9995-02fb245ca93d"; # lsblk -f (root UUID)
   powerManagement.enable = true;
   swapDevices = [
-#    {
-#      device = "/var/lib/swapfile";
-#      size = 16 * 1024; # 36GB in MB
-#    }
+    {
+      device = "/var/lib/swapfile";
+      size = 25 * 1024; # in MB
+    }
   ];
   # Hibernation - End
 
