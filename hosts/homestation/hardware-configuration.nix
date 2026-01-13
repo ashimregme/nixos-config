dd963db0a07257c088ba01927c8d8039aa26f1bf
Ashim Regmi <5734294+ashimregme@users.noreply.github.com>
2026-01-13 10:51:05 +0545

    enable zswap

--- hosts/homestation/hardware-configuration.nix
+++ hosts/homestation/hardware-configuration.nix
@@ -25,7 +25,12 @@
   # Hibernation - Start
   boot.kernelParams = [
     "resume_offset=28624896" # sudo filefrag -v /var/lib/swapfile | head
+    "zswap.enabled=1" # enables zswap
+    "zswap.compressor=lz4" # compression algorithm
+    "zswap.max_pool_percent=20" # maximum percentage of RAM that zswap is allowed to use
+    "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
   ];
+  boot.initrd.systemd.enable=true;
   boot.resumeDevice = "/dev/disk/by-uuid/3fc474fa-2e1a-480c-9995-02fb245ca93d"; # lsblk -f (root UUID)
   powerManagement.enable = true;
   swapDevices = [
